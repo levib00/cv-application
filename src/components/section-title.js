953511b5b8a0f4e0a8ce5c896b2e6227067d9be0
levib00 <levib8@hotmail.com>
2023-02-12 19:15:46 -0500

    Add button to add experience

--- src/components/section-title.js
+++ src/components/section-title.js
@@ -3,16 +3,25 @@ import React from "react";
 class SectionHeader extends React.Component {
   constructor(props) {
     super(props)
+    this.handleAdd = this.handleAdd.bind(this)
+    this.state = {
+      clicked: false
+    }
+  }
+  
+  handleAdd() {
+    this.props.addItem(this.props.historyArray);
   }
   
   render() {
+    const clicked = this.state.clicked
     return (
       <div>
         <h3>{this.props.title}</h3>
+        <button onClick={!clicked ? this.handleAdd : null}>Add +</button>
       </div>
     )
   }
-
 }
 
 export default SectionHeader;
