88f31201133c89d623a01fd47ae02259bc552467
levib00 <levib8@hotmail.com>
2023-02-14 22:51:47 -0500

    Add edit and remove buttons

--- src/components/entry.js
+++ src/components/entry.js
@@ -5,6 +5,8 @@ class Entry extends React.Component {
     const { role, company, responsibilities } = this.props.info
     return (
       <div key={this.props.listKey}>
+        <button onClick={this.props.remove}>Remove</button>
+        <button onClick={this.props.edit}>edit</button> {/*// TODO: give the current text thats in the entry to the edit boxes when this is pressed. */}
         <div>
           {role} - {company}
         </div>
