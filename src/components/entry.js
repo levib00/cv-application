6a8431a106db7fa99af86ab405427211a49fef6f
levib00 <levib8@hotmail.com>
2023-02-17 16:47:33 -0500

    Change date to 'present' if the date is the current date.

--- src/components/entry.js
+++ src/components/entry.js
@@ -5,6 +5,7 @@ class Entry extends React.Component {
     super(props);
     this.handleEdit = this.handleEdit.bind(this);
     this.handleRemove = this.handleRemove.bind(this);
+    this.checkIfPresent = this.checkIfPresent.bind(this);
   }
 
   handleEdit(e) {
@@ -15,15 +16,32 @@ class Entry extends React.Component {
     this.props.remove(e, this.props.whichArray)
   }
 
+  checkIfPresent(endDate) {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+
+    const dateString = year + '-' + ('0' + (month)).slice(-2) + '-' + ('0' + day).slice(-2);
+
+    if (endDate === dateString) {
+      console.log('present')
+      return 'present'
+    } else {
+      console.log(`${dateString}`, '__', endDate)
+      //console.log(`${year}-${month}-${day}`, '__', endDate)
+      return endDate
+    }
+  }
+
   render() {
     const { role, company, responsibilities, startDate, endDate } = this.props.info
-    console.log(this.props.info)
     return (
       <div key={this.props.listKey}>
         <button onClick={this.handleRemove}>Remove</button>
-        <button onClick={this.handleEdit}>edit</button> {/*// TODO: give the current text thats in the entry to the edit boxes when this is pressed. */}
+        <button onClick={this.handleEdit}>edit</button> {/*// TODO: 4. give the current text thats in the entry to the edit boxes when this is pressed. do the same for personalInfo */}
         <div>
-          <div>{role} - {company}</div> <div>{startDate} - {endDate}</div>
+          <div>{role} - {company}</div> <div>{startDate} - {this.checkIfPresent(endDate)}</div>
         </div>
         <p>{responsibilities}</p>
       </div>
